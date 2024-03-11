37355b5c965a0693ece9f04f2affb58d154784e7
pintongc <144455500+pintongc@users.noreply.github.com>
2024-03-11 03:49:09 -0400

    12

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CartView from '../views/CartView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/cart', component: CartView }
 ]
 
 const router = createRouter({
